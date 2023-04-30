1d5368dc955df271abb0080508a0385643aa5e1d
Sam Chung <samc@seek.com.au>
2023-04-30 14:07:56 +1000

    restore formatting

--- src/openapi3-ts/dist/model/oas-common.ts
+++ src/openapi3-ts/dist/model/oas-common.ts
@@ -1,22 +1,15 @@
 import { ISpecificationExtension } from './specification-extension';
 export interface ServerObject extends ISpecificationExtension {
-  url: string;
-  description?: string;
-  variables?: {
-    [v: string]: ServerVariableObject;
-  };
+    url: string;
+    description?: string;
+    variables?: {
+        [v: string]: ServerVariableObject;
+    };
 }
 export interface ServerVariableObject extends ISpecificationExtension {
-  enum?: string[] | boolean[] | number[];
-  default: string | boolean | number;
-  description?: string;
+    enum?: string[] | boolean[] | number[];
+    default: string | boolean | number;
+    description?: string;
 }
-export declare function getExtension(
-  obj: ISpecificationExtension | undefined,
-  extensionName: string,
-): any;
-export declare function addExtension(
-  obj: ISpecificationExtension | undefined,
-  extensionName: string,
-  extension: any,
-): void;
+export declare function getExtension(obj: ISpecificationExtension | undefined, extensionName: string): any;
+export declare function addExtension(obj: ISpecificationExtension | undefined, extensionName: string, extension: any): void;
