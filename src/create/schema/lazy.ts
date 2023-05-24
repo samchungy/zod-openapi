3165ffca8da66ad9dffdf9d2fb39b0bc371862d6
Sam Chung <samc@seek.com.au>
2023-05-24 17:02:52 +1000

    Allow inner register of ZodLazy

--- src/create/schema/lazy.ts
+++ src/create/schema/lazy.ts
@@ -1,7 +1,6 @@
 import type { ZodLazy, ZodType } from 'zod';
 
 import type { oas31 } from '../../openapi3-ts/dist';
-import { createComponentSchemaRef } from '../components';
 
 import { type SchemaState, createSchemaOrRef } from '.';
 
@@ -9,21 +8,34 @@ export const createLazySchema = (
   zodLazy: ZodLazy<any>,
   state: SchemaState,
 ): oas31.ReferenceObject | oas31.SchemaObject => {
+  const innerSchema = zodLazy._def.getter() as ZodType;
   const component = state.components.schemas.get(zodLazy);
   const ref = component?.ref ?? zodLazy._def.openapi?.ref;
-  if (!ref) {
-    throw new Error(`Please register the ${JSON.stringify(zodLazy._def)} type`);
-  }
 
-  if (!component || component.type === 'partial') {
+  if (ref && (!component || component.type === 'partial')) {
     state.components.schemas.set(zodLazy, {
       type: 'lazy',
       ref,
     });
-    return createSchemaOrRef(zodLazy._def.getter() as ZodType, state);
+    return createSchemaOrRef(innerSchema, state);
   }
 
-  return {
-    $ref: createComponentSchemaRef(ref),
-  };
+  const innerComponent = state.components.schemas.get(zodLazy);
+  const innerRef = innerComponent?.ref ?? innerSchema._def.openapi?.ref;
+
+  if (innerRef && (!innerComponent || innerComponent.type === 'partial')) {
+    state.components.schemas.set(innerSchema, {
+      type: 'lazy',
+      ref: innerRef,
+    });
+    return createSchemaOrRef(innerSchema, state);
+  }
+
+  throw new Error(
+    `The ZodLazy ${JSON.stringify(
+      zodLazy._def,
+    )} or Schema within ZodLazy ${JSON.stringify(
+      innerSchema._def,
+    )} must be registered`,
+  );
 };
