4f21cbc839407b9999976ae40ac05d22cee327da
Sam Chung <samc@seek.com.au>
2023-05-24 12:40:30 +1000

    Ad optional effect support

--- src/create/schema/optional.test.ts
+++ src/create/schema/optional.test.ts
@@ -2,9 +2,9 @@ import { z } from 'zod';
 
 import { extendZodWithOpenApi } from '../../extendZod';
 import type { oas31 } from '../../openapi3-ts/dist';
-import { createOutputState } from '../../testing/state';
+import { createInputState, createOutputState } from '../../testing/state';
 
-import { createOptionalSchema } from './optional';
+import { createOptionalSchema, isOptionalSchema } from './optional';
 
 extendZodWithOpenApi(z);
 
@@ -18,5 +18,85 @@ describe('createOptionalSchema', () => {
     const result = createOptionalSchema(schema, createOutputState());
 
     expect(result).toStrictEqual(expected);
+  });
+});
+
+describe('isOptionalSchema', () => {
+  it('returns true for an optional string', () => {
+    const schema = z.string().optional();
+
+    const result = isOptionalSchema(schema, createOutputState());
+
+    expect(result).toBe(true);
+  });
+
+  it('returns true for an optional transform', () => {
+    const schema = z
+      .string()
+      .optional()
+      .transform((str) => str?.length ?? 0);
+
+    const result = isOptionalSchema(schema, createOutputState());
+
+    expect(result).toBe(true);
+  });
+
+  it('returns true for a union with an optional', () => {
+    const schema = z.union([z.string(), z.string().optional()]);
+
+    const result = isOptionalSchema(schema, createOutputState());
+
+    expect(result).toBe(true);
+  });
+
+  it('returns true for an intersection with an optional', () => {
+    const schema = z.intersection(z.string(), z.string().optional());
+
+    const result = isOptionalSchema(schema, createOutputState());
+
+    expect(result).toBe(true);
+  });
+
+  it('returns true for a nullable with an optional', () => {
+    const schema = z.string().optional().nullable();
+
+    const result = isOptionalSchema(schema, createOutputState());
+
+    expect(result).toBe(true);
+  });
+
+  it('returns true for an async transform with an optional', () => {
+    const schema = z
+      .string()
+      .optional()
+      // eslint-disable-next-line @typescript-eslint/require-await
+      .transform(async () => 'x');
+
+    const result = isOptionalSchema(schema, createOutputState());
+
+    expect(result).toBe(true);
+  });
+
+  it('returns true for an output state on pipeline', () => {
+    const schema = z
+      .string()
+      .transform((str) => str.length)
+      .pipe(z.number().optional());
+
+    const result = isOptionalSchema(schema, createOutputState());
+
+    expect(result).toBe(true);
+  });
+
+  it('returns true for an input state on pipeline', () => {
+    const schema = z
+      .string()
+      .optional()
+      .transform((str) => str?.length ?? 0)
+      .pipe(z.number());
+
+    const result = isOptionalSchema(schema, createInputState());
+
+    expect(result).toBe(true);
   });
 });
--- src/create/schema/optional.ts
+++ src/create/schema/optional.ts
@@ -1,4 +1,13 @@
-import type { ZodOptional, ZodTypeAny } from 'zod';
+import {
+  ZodDefault,
+  ZodEffects,
+  ZodIntersection,
+  ZodNullable,
+  type ZodOptional,
+  ZodPipeline,
+  type ZodTypeAny,
+  ZodUnion,
+} from 'zod';
 
 import type { oas31 } from '../../openapi3-ts/dist';
 
@@ -10,3 +19,50 @@ export const createOptionalSchema = (
 ): oas31.SchemaObject | oas31.ReferenceObject =>
   // Optional doesn't change OpenAPI schema
   createSchemaOrRef(zodOptional.unwrap() as ZodTypeAny, state);
+
+export const isOptionalSchema = (
+  zodSchema: ZodTypeAny,
+  state: SchemaState,
+): boolean => {
+  if (zodSchema instanceof ZodEffects) {
+    return isOptionalSchema(zodSchema._def.schema as ZodTypeAny, state);
+  }
+
+  if (zodSchema instanceof ZodDefault) {
+    return isOptionalSchema(zodSchema._def.innerType as ZodTypeAny, state);
+  }
+
+  if (zodSchema instanceof ZodUnion) {
+    return (zodSchema._def.options as ZodTypeAny[]).some((schema) =>
+      isOptionalSchema(schema, state),
+    );
+  }
+
+  if (zodSchema instanceof ZodIntersection) {
+    return [zodSchema._def.left, zodSchema._def.right].some((schema) =>
+      isOptionalSchema(schema as ZodTypeAny, state),
+    );
+  }
+
+  if (zodSchema instanceof ZodPipeline) {
+    if (
+      state.effectType === 'input' ||
+      (state.type === 'input' && state.effectType !== 'output')
+    ) {
+      return isOptionalSchema(zodSchema._def.in as ZodTypeAny, state);
+    }
+
+    if (
+      state.effectType === 'output' ||
+      (state.type === 'output' && state.effectType !== 'input')
+    ) {
+      return isOptionalSchema(zodSchema._def.out as ZodTypeAny, state);
+    }
+  }
+
+  if (zodSchema instanceof ZodNullable || zodSchema instanceof ZodDefault) {
+    return isOptionalSchema(zodSchema._def.innerType as ZodTypeAny, state);
+  }
+
+  return zodSchema.isOptional();
+};
