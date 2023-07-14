4860d2a1365aaa213486a53d8a48ac8bbd24a9db
Sam Chung <samchungy@gmail.com>
2023-07-14 13:02:03 +1000

    Add ZodRecord key type support (#136)

--- src/create/schema/record.test.ts
+++ src/create/schema/record.test.ts
@@ -2,14 +2,17 @@ import { z } from 'zod';
 
 import { extendZodWithOpenApi } from '../../extendZod';
 import type { oas31 } from '../../openapi3-ts/dist';
-import { createOutputState } from '../../testing/state';
+import {
+  createOutputOpenapi3State,
+  createOutputState,
+} from '../../testing/state';
 
 import { createRecordSchema } from './record';
 
 extendZodWithOpenApi(z);
 
 describe('createRecordSchema', () => {
-  it('creates an object schema with additional properties', () => {
+  it('creates an object schema with additional properties in 3.0.0', () => {
     const expected: oas31.SchemaObject = {
       type: 'object',
       additionalProperties: {
@@ -19,6 +22,41 @@ describe('createRecordSchema', () => {
     const schema = z.record(z.string());
 
     const result = createRecordSchema(schema, createOutputState());
+
+    expect(result).toStrictEqual(expected);
+  });
+
+  it('creates an object schema with propertyNames in 3.1.0', () => {
+    const expected: oas31.SchemaObject = {
+      type: 'object',
+      // @ts-expect-error FIXME: https://github.com/metadevpro/openapi3-ts/pull/120
+      propertyNames: {
+        type: 'string',
+        pattern: '^foo',
+      },
+      additionalProperties: {
+        type: 'string',
+      },
+    };
+    const schema = z.record(z.string().regex(/^foo/), z.string());
+
+    const result = createRecordSchema(schema, createOutputState());
+
+    expect(result).toStrictEqual(expected);
+  });
+
+  it('creates an object schema with additional properties and key properties in 3.0.0', () => {
+    const expected: oas31.SchemaObject = {
+      type: 'object',
+      properties: {
+        a: { type: 'string' },
+        b: { type: 'string' },
+      },
+      additionalProperties: false,
+    };
+    const schema = z.record(z.enum(['a', 'b']), z.string());
+
+    const result = createRecordSchema(schema, createOutputOpenapi3State());
 
     expect(result).toStrictEqual(expected);
   });
