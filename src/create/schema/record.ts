fa4147f0595864d08ff5f27bba7d37ce0fe185bd
samchungy <samchungy@gmail.com>
2023-07-14 12:07:49 +1000

    Add record key type support

--- src/create/schema/record.ts
+++ src/create/schema/record.ts
@@ -1,5 +1,6 @@
-import type { ZodRecord, ZodTypeAny } from 'zod';
+import { ZodEnum, type ZodRecord, type ZodTypeAny } from 'zod';
 
+import { satisfiesVersion } from '../../openapi';
 import type { oas31 } from '../../openapi3-ts/dist';
 
 import { type SchemaState, createSchemaOrRef } from '.';
@@ -7,11 +8,41 @@ import { type SchemaState, createSchemaOrRef } from '.';
 export const createRecordSchema = (
   zodRecord: ZodRecord<any, any>,
   state: SchemaState,
-): oas31.SchemaObject => ({
-  type: 'object',
-  additionalProperties: createSchemaOrRef(
+): oas31.SchemaObject => {
+  const additionalProperties = createSchemaOrRef(
     zodRecord.valueSchema as ZodTypeAny,
     state,
     ['record value'],
-  ),
-});
+  );
+
+  if (zodRecord.keySchema instanceof ZodEnum) {
+    return {
+      type: 'object',
+      properties: (zodRecord.keySchema._def.values as string[]).reduce<
+        NonNullable<oas31.SchemaObject['properties']>
+      >((acc, key) => {
+        acc[key] = additionalProperties;
+        return acc;
+      }, {}),
+      additionalProperties: false,
+    };
+  }
+
+  if (satisfiesVersion(state.components.openapi, '3.1.0')) {
+    return {
+      type: 'object',
+      // @ts-expect-error FIXME: https://github.com/metadevpro/openapi3-ts/pull/120
+      propertyNames: createSchemaOrRef(
+        zodRecord.keySchema as ZodTypeAny,
+        state,
+        ['record key'],
+      ),
+      additionalProperties,
+    };
+  }
+
+  return {
+    type: 'object',
+    additionalProperties,
+  };
+};
