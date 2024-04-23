0022ef6e3bad6a9035e9a80ff5b9c4e91df313cf
samchungy <samchungy@gmail.com>
2024-04-23 14:27:52 +1000

    Fix Type Exports

--- src/extend.ts
+++ src/extend.ts
@@ -4,4 +4,4 @@ import { extendZodWithOpenApi } from './extendZod';
 
 extendZodWithOpenApi(z);
 
-export type * from './extendZod';
+export * from './extendZodTypes'; // compatibility with < TS 5.0 as the export type * syntax is not supported
