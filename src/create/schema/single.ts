cb3cf471bdae69dc9de317359912d86efb4afd67
samchungy <samchungy@gmail.com>
2024-09-24 14:42:01 +1000

    Add type comments

--- src/create/schema/single.ts
+++ src/create/schema/single.ts
@@ -2,7 +2,11 @@ import type { ZodType } from 'zod';
 
 import type { oas30, oas31 } from '../../../dist';
 import type { OpenApiVersion } from '../../openapi';
-import { createSchemaComponents, getDefaultComponents } from '../components';
+import {
+  type CreationType,
+  createSchemaComponents,
+  getDefaultComponents,
+} from '../components';
 
 import { type SchemaState, createSchema as internalCreateSchema } from '.';
 
@@ -18,10 +22,16 @@ export interface SchemaResult {
 
 export interface CreateSchemaOpts {
   /**
-   * The type of schema to create, defaults to 'output'
+   * This controls whether this should be rendered as a request (`input`) or response (`output`). Defaults to `output`
    */
-  schemaType?: 'input' | 'output';
+  schemaType?: CreationType;
+  /**
+   * OpenAPI version to use, defaults to `'3.1.0'`
+   */
   openapi?: OpenApiVersion;
+  /**
+   * Additional components to use and create while rendering the schema
+   */
   components?: Record<string, ZodType>;
 }
 
