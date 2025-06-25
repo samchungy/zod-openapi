28a5f5d29bf02113ec3504b609396a8ecd7d3cac
samchungy <samchungy@gmail.com>
2025-06-25 23:07:58 +1000

    update exports

--- src/entries/api.ts
+++ src/entries/api.ts
@@ -2,3 +2,4 @@ export { createComponents, createRegistry } from '../create/components';
 export { createMediaTypeObject } from '../create/content';
 export { createParameter } from '../create/parameters';
 export { unwrapZodObject } from '../create/object';
+export type * from '../zod';
