0467bcc9872c89d3359db6d3d52308a75f4f2967
samchungy <samchungy@gmail.com>
2025-06-26 18:48:56 +1000

    delete zod-openapi eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,15 +5,4 @@ module.exports = [
     ignores: ['src/openapi3-ts/*', '**/crackle.config.ts', 'api', 'extend'],
   },
   ...skuba,
-  {
-    files: ['examples/**/*/types/**/*.ts'],
-
-    rules: {
-      'zod-openapi/require-openapi': 'error',
-      'zod-openapi/require-comment': 'error',
-      'zod-openapi/require-example': 'error',
-      'zod-openapi/prefer-openapi-last': 'error',
-      'zod-openapi/prefer-zod-default': 'error',
-    },
-  },
 ];
