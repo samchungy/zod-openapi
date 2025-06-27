77247db3402c73efc31836a9f7693d9cd5d09307
samchungy <samchungy@gmail.com>
2025-06-27 16:54:19 +1000

    export isAnyZodType

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,4 @@
 export { createComponents, createRegistry } from './create/components';
-export { createMediaTypeObject } from './create/content';
 export { createParameter } from './create/parameters';
 export { unwrapZodObject } from './create/object';
-export type * from './zod';
+export * from './zod';
